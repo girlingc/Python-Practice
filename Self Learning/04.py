fd44b445f6c6eb558aca825d8d4f0c553cf363de
Christian Girling <girling.christian@gmail.com>
2022-04-29 21:36:33 -0700

    04, 05

---
+++ Self Learning/04.py
@@ -0,0 +1,8 @@
+birth_year = input('Birth year: ')
+print(type(birth_year))
+age = 2020 - int(birth_year)
+print(type(age))
+print(age)
+weight_lbs = input('How much do you weigh (lbs)? ')
+weight_kg = int(weight_lbs) / int(2.2)
+print(weight_kg)
---
+++ Self Learning/05.py
@@ -0,0 +1,7 @@
+course = '''Python's Course for "Beginners"
+Thanks everyone for your help
+it is appreciated'''
+another = course[:]
+print(course[0:10])
+print(another)
+# square brackets reads characters individually
