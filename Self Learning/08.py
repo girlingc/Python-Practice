d89dc9b7587732e5ce5fa51d872fa7dcacd5d769
Christian Girling <girling.christian@gmail.com>
2022-04-29 22:06:57 -0700

    08, 09

---
+++ Self Learning/08.py
@@ -0,0 +1,11 @@
+course = 'Python for Beginners'
+print(len(course))
+print(course.upper())
+print(course)
+print(course.title())
+print(course.lower())
+print(course.find('P'))
+print(course.find('e'))
+print(course.replace('Beginners', 'Absolute Beginners'))
+print('Python' in course)
+print('python' in course)
---
+++ Self Learning/09.py
@@ -0,0 +1,18 @@
+# divide int
+print(10 // 3)
+# divide float
+print(10 / 3)
+# divide remainder of divisions
+print(10%3)
+# exponent
+print(10**3)
+
+x = 10
+x = x +3
+print(x)
+
+# Augmented assignment operator
+x += 3
+print(x)
+x-=3
+print(x)
