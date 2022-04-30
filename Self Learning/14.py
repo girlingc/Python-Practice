0c96352d5839457f5a35b898eb7bc9ed7e4f2c33
Christian Girling <girling.christian@gmail.com>
2022-04-29 22:44:47 -0700

    14, 13

---
+++ Self Learning/14.py
@@ -0,0 +1,11 @@
+Temperature = 30
+
+if Temperature >= 30:
+    print("It's a hot day")
+elif Temperature == 25:
+    print("It's 25 degrees outside today")
+elif Temperature <10:
+    print("It's a cold day")
+else: print("It's neither hot nor cold today")
+
+
